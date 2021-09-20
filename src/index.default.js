e8581678089a5b66742baccf70bbefd81da7ae00
Ciffelia <mc.prince.0203@gmail.com>
2021-09-20 21:22:58 +0900

    Try to look for global object

--- src/index.default.js
+++ src/index.default.js
@@ -2,13 +2,28 @@
 // The original file is available at https://github.com/node-fetch/node-fetch/blob/152214ca2f6e2a5a17d71e4638114625d3be30c6/browser.js
 // and the license of node-fetch is available at https://github.com/node-fetch/node-fetch/blob/152214ca2f6e2a5a17d71e4638114625d3be30c6/LICENSE.md
 
-module.exports = exports = globalThis.fetch;
+// Global object
+let g
 
-// Needed for TypeScript and Webpack.
-if (globalThis.fetch) {
-  exports.default = globalThis.fetch.bind(globalThis);
+if (typeof globalThis !== 'undefined') {
+  g = globalThis
+} else if (typeof self !== 'undefined') {
+  g = self
+} else if (typeof window !== 'undefined') {
+  g = window
+} else if (typeof global !== 'undefined') {
+  g = global
+} else {
+  throw new Error('Unable to locate global object')
 }
 
-exports.Headers = globalThis.Headers;
-exports.Request = globalThis.Request;
-exports.Response = globalThis.Response;
+module.exports = exports = g.fetch;
+
+// Needed for TypeScript and Webpack.
+if (g.fetch) {
+  exports.default = g.fetch.bind(g);
+}
+
+exports.Headers = g.Headers;
+exports.Request = g.Request;
+exports.Response = g.Response;
